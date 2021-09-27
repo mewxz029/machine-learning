cb958e4e7c0385972c3400ad87fda1a9aff495d3
mewxz029 <mewlawbreakertv@gmail.com>
2021-09-27 17:08:56 +0700

    change some code

--- digits.py
+++ digits.py
@@ -2,6 +2,6 @@ import pylab
 from sklearn import datasets
 digits_dataset = datasets.load_digits()
 print(digits_dataset.keys())
-print(digits_dataset['target_names'])
+print(digits_dataset.target[:10])
 pylab.imshow(digits_dataset.images[1], cmap = pylab.cm.gray_r)
 pylab.show()
