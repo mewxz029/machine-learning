984924028fac3f6d42481fef9f2a40477b6a8944
mewxz029 <mewlawbreakertv@gmail.com>
2021-09-29 22:12:50 +0700

    change some code

--- ML1.py
+++ ML1.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# create Array number sample space => -5 to 5 length = 10
 x = np.linspace(-5,5,10)
 print(x)
 y = 2*x+1
 
+plt.scatter(x, y, color='black')
 plt.plot(x, y, 'r', label='y = 2*x+1')
 plt.xlabel('x')
 plt.ylabel('y')
